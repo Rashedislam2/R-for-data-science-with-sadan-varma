3a74039fc5979ab431bcec05f91eb7902d0cb254
Rashedislam2 <www.rashedmd234@gmail.com>
2025-04-30 22:50:58 +0600

    rdsmlday3

---
+++ RDSML-Day-3 👉 R Programming Basics (Part-1)/r-basics-part1.R
@@ -0,0 +1,10 @@
+#calculating some health parameter
+my_age = 97
+my_weight = 60
+my_height = 65
+x = (my_height * my_age) / my_weight
+print(x)
+
+
+students_height= c (50,55,60,14,25,36)
+mean(students_height)
