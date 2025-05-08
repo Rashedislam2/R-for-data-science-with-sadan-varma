69af43e5f417c496d913515cbeba107d69d6471b
Rashedislam2 <www.rashedmd234@gmail.com>
2025-05-08 16:32:56 +0600

    day 4 R basics part 2

---
+++ RDSML-Day-4 👉 R Programming Basics (Part-2)/r basics part 2.R
@@ -0,0 +1,60 @@
+ a= 11 
+b = 7.19 
+class(a)
+class(b)
+
+
+name = 'nbict lab'
+print(name)
+class(name) 
+
+c='52'
+print(c+9)
+print(a+9)
+
+
+a
+b
+class(a<b)
+print(a<b)
+
+
+
+x= 19
+y= 22-3
+print(x==y)
+print(x!=y)
+print(y>x)
+print(x>=y)
+
+
+#logical operator
+# && logical operator
+#|| logical operator
+4==4 && 5==5
+4==3 && 5==5
+4==3 && 5==6
+
+
+x
+y
+x!=y || x==y
+x==y || x==y
+x==y||x!=y
+x!=y||x!=y
+
+
+# simple mathematical in R
+
+z =6+9-8*(2/3)
+print(z)
+
+
+w=(4+3-3)*2/3
+print(w)
+
+y =4^4
+print(y)
+11/5
+11%%5
+11%/%5
