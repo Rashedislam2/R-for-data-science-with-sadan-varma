f5ec9d61933f2881f1973a43a445254040557478
Rashedislam2 <www.rashedmd234@gmail.com>
2025-05-17 21:01:53 +0600

    list

--- RDSML-Day-6 👉 Factors and Lists in R/Factors and Lists in R.R
+++ RDSML-Day-6 👉 Factors and Lists in R/Factors and Lists in R.R
@@ -16,3 +16,30 @@ birth_month_fact = factor(birth_month,
                           ordered = TRUE,
                           labels = c("jan","feb",'mar','apr',"may",'jun','jul','aug',"sep","oct","nov","dec"))
 summary(birth_month_fact)
+
+
+#list in R 
+#lists are used to place number of items in bundle
+
+a = c(2,4,7)
+b = c('red',"green","blue")
+c = "welcome!"
+
+my_list = list(a,b,c)
+my_list
+
+#naming the list items
+
+my_list = list(Pieces=a,Colors=b,Message=c)
+my_list
+
+#CALLIng a specific data structure
+my_list[3]
+my_list['Colors']
+my_list$Pieces
+my_list$Colors
+
+#calling specific items from data structure
+
+my_list$Colors[2]
+my_list$Colors[3]
