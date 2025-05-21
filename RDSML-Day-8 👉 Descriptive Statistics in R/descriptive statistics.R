181e42d2dff934f9fe5628813dc099907b9132b3
Rashedislam2 <www.rashedmd234@gmail.com>
2025-05-21 22:56:18 +0600

    descriptive statistics

---
+++ RDSML-Day-8 👉 Descriptive Statistics in R/descriptive statistics.R
@@ -0,0 +1,12 @@
+#Descriptive statistics using psych package
+
+install.packages("psych")
+library(psych)
+
+data()
+AirPassengers  
+BOD
+#using the new york air port data
+
+install.packages("nycfights13")
+attach(nycflights13::flights)
