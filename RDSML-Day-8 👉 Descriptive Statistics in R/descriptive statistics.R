d41147b101bca180fec765b17adfa9b4cf3f2044
Rashedislam2 <www.rashedmd234@gmail.com>
2025-05-23 20:38:14 +0600

    descriptive statistics

--- RDSML-Day-8 👉 Descriptive Statistics in R/descriptive statistics.R
+++ RDSML-Day-8 👉 Descriptive Statistics in R/descriptive statistics.R
@@ -1,1 +1,21 @@
+install.packages("psych")
+library("psych")
 
+data()
+AirPassengers
+
+
+
+#using the New York airport flight data
+install.packages("nycflights13")
+attach(nycflights13::flights)
+
+#lets look at the first 6 record using head()function
+head(nycflights13::flights)
+
+summary(distance)
+describe(distance)
+
+demo = cbind(arr_delay,dep_delay,distance)
+demo         
+describe(demo)
