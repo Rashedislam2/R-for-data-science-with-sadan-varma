9805ce3f99c61934b48bb4269b79d8f046a32c18
Rashedislam2 <www.rashedmd234@gmail.com>
2025-06-18 23:01:29 +0600

    histogram 2

--- RDSML-Day-14 👉 Histogram in R (Part-2)/histogram 2.R
+++ RDSML-Day-14 👉 Histogram in R (Part-2)/histogram 2.R
@@ -26,8 +26,24 @@ library(plyr)
 group_means=ddply(dataset,"sex",summarise,grp.mean=mean(weight))
 group_means
 
+#adding mean lines
+myplot = ggplot(dataset,aes(x=weight,fill=sex))+
+  geom_histogram(color="white",alpha=1,position = "dodge")+
+  geom_vline(data=group_means,aes(xintercept = grp.mean,color=sex),
+             linetype="dashed")+
+  theme(legend.position = "top")
+myplot
 
+#using custom color paletters
+myplot + scale_color_manual(values = c("#999999","#E69F00"))+
+  scale_fill_manual(values = c("#999999","#E69F00"))
 
+#using color brewer color paletters
+myplot+scale_color_brewer((palette="Dark2"))+
+  scale_fill_brewer(palette = "Dark2")
+#using a grey scale
+myplot+scale_color_grey()+ scale_fill_grey()+
+  theme_classic()
 
 
 
