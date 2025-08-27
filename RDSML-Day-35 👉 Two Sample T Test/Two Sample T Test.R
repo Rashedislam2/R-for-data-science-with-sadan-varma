4130076e8261958cbb64c5dd4a5c27689e251e9a
Rashedislam2 <www.rashedmd234@gmail.com>
2025-08-27 22:13:30 +0600

    independent_t_test

---
+++ RDSML-Day-35 👉 Two Sample T Test/Two Sample T Test.R
@@ -0,0 +1,13 @@
+#two sample t test
+#importing data set
+dataset = read.csv("scores.csv")
+
+#checking normality
+shapiro.test(dataset$Male)
+shapiro.test(dataset$Female)
+
+#checking homogeinity of the variance
+var.test(x=dataset$Male,y=dataset$Female)
+
+#running the independent sample t test
+t.test(x=dataset$Male,Y=dataset$Female,var.equal = TRUE)
