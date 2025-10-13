cd75238acab513cd8bbca55401fae14c24ca1921
Md. Rashedul islam <www.rashedmd234@gmail.com>
2025-10-13 22:46:02 +0600

    simple linear reg 2

---
+++ RDSML-Day-47 👉 Simple Linear Regression in R (Part-2)/Simple Linear Regression in R (Part-2).R
@@ -0,0 +1,54 @@
+#importing the data set
+dataset= read.csv("Salary_Data.csv")
+dataset
+View(dataset)
+#Splitting the dataset into the Training set and Test set
+#install.packages("caTools")
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary,SplitRatio = 2/3)
+split
+training_set = subset(dataset,split == TRUE)
+training_set
+View(training_set)
+test_set = subset(dataset,split == FALSE)
+View(test_set)
+#fitting the simple linear regression model to the training set
+regressor = lm(formula =Salary~YearsExperience ,
+               data =training_set )
+regressor
+
+#checking the summary of the model
+summary(regressor)
+
+# predicitng the test set results
+y_pred = predict(regressor,newdata = test_set)
+y_pred
+#visualizing the training set results
+#install.packages("ggplot2")
+
+library(ggplot2)
+ggplot()+
+  geom_point(aes(x=training_set$YearsExperience,y=training_set$Salary),
+                 colour = "red") +
+geom_point(aes(x=training_set$YearsExperience,y=y_pred),
+           colour = "blue")
+
+ggplot()+
+  geom_point(aes(x=training_set$YearsExperience,y=training_set$Salary),
+             colour = "red") +
+  geom_line(aes(x=training_set$YearsExperience,y=y_pred),
+             colour = "blue")+
+  ggtitle("Salary vs Experience") +
+  xlab("Years of experience")+
+  ylab("Salary")
+
+#visualizing the test set results
+ggplot()+
+  geom_point(aes(x=test_set$YearsExperience,y=test_set$Salary),
+             colour = "red") +
+  geom_line(aes(x=test_set$YearsExperience,y = predict(regressor,newdata = test_set)),
+            colour = "blue")+
+  ggtitle("Salary vs Experience(test test)") +
+  xlab("Years of experience")+
+  ylab("Salary")
